2c74b8955185e8deb57aa9026e882d62211c6e6f
aarpitsharmaa27 <arpit8894350818@gmail.com>
2026-01-17 00:33:35 +0530

    Add string slicing python program

---
+++ String_slicing.py
@@ -0,0 +1,13 @@
+a = "My name is Arpit Sharma"
+b = "0123456789"
+print(a)
+print(b)
+print(a[11:23])
+print(a[3:8])
+print(a[0:23])
+print(a[-6:])
+
+print(b[::2])    # print with gap of 2
+print(b[::3])    # print with gap of 4
+print(b[::-1])   # reverse the string
+print(b[7::-1])

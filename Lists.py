4599a771f86312fc176c2b27e4c96ebaad93131e
aarpitsharmaa27 <aarpitsharmaa27@gmail.com>
2026-02-03 00:54:40 +0530

    List in Python

--- Lists.py
+++ Lists.py
@@ -2,17 +2,18 @@ names = ["Arpit", "Brt", "Crt", "Drt", "Ert"]
 print(names)  
 print(type(names))
 
-print(names[0])
-print(names[1])
-print(names[2])
+print(names[0])     #  First element
+print(names[1]) #  Second element
+print(names[2]) #  Third element
 
-print(names[-1])
+print(names[-1]) #  Last element
 
-print(names[1:3])
+print(names[1:3]) # From index 1 to index 3 (excluding index 3)
 print(names[:2])  # Every second name
-print(names[::2])
+print(names[::2]) # Every name from index 2 to end
 print(names[-1:]) # Last name
+print(names[-2::])
 
 print(names[::-1]) # Reversing the list
 
-print(names[-1:-6:-2])
+print(names[-1:-6:-2]) # Starting from the last name, moving backwards, every second name

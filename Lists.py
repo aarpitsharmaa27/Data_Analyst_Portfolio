83a961e19a5606e2b2c59ac4bccf09b48cef2691
aarpitsharmaa27 <aarpitsharmaa27@gmail.com>
2026-02-03 00:52:37 +0530

    List in Pytho

--- Lists.py
+++ Lists.py
@@ -1,1 +1,18 @@
-print("Hello, World!")
+names = ["Arpit", "Brt", "Crt", "Drt", "Ert"]
+print(names)  
+print(type(names))
+
+print(names[0])
+print(names[1])
+print(names[2])
+
+print(names[-1])
+
+print(names[1:3])
+print(names[:2])  # Every second name
+print(names[::2])
+print(names[-1:]) # Last name
+
+print(names[::-1]) # Reversing the list
+
+print(names[-1:-6:-2])

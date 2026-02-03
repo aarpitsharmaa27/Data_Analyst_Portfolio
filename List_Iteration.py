bc7118a847243aa0330664410fade2bc6fe7b2d8
aarpitsharmaa27 <aarpitsharmaa27@gmail.com>
2026-02-03 21:36:02 +0530

    List Iteration in Python

--- List_Iteration.py
+++ List_Iteration.py
@@ -1,3 +1,20 @@
 a = ["Arpit", "Harsh", "Keshav", "Ashish"]
 for i in a:
-    print(i)
+    print(i)  # Iteration using loop
+
+
+#Iteration using range and len function 
+for i in range(len(a)):
+    print(a[i])
+
+
+
+# Iteration using while loop
+i = 0
+while i < len(a):
+    print(a[i])
+    i += 1
+
+
+#Using short hand for loop
+[print (i) for i in a]

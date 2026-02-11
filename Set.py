87a7581f723b1f9b8f2a103050d35eae113759fd
aarpitsharmaa27 <aarpitsharmaa27@gmail.com>
2026-02-12 00:43:20 +0530

    Sets in Python

--- Set.py
+++ Set.py
@@ -1,1 +1,21 @@
-print('hakngn')
+a = {"Arpit", "Harsh", "Ashish", "Keshav"}
+print(a)
+print(type(a))
+
+
+# SET FUNCTIONS
+
+a.add("Tarun")
+print(a)
+
+a.pop()
+print(a)
+
+a.remove("Ashish")
+print(a)
+
+a.discard('Keshav')
+print(a)
+
+b = a.copy
+print(b)

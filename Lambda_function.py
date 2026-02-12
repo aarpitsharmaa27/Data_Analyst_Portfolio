5c5bc97eb1d50bb81b3477babdaf3f9bd9cf3734
aarpitsharmaa27 <aarpitsharmaa27@gmail.com>
2026-02-13 00:23:22 +0530

    lambda function in python

---
+++ Lambda_function.py
@@ -0,0 +1,13 @@
+# Lambda is used when as anonymous function is required for a
+#                       short period on time
+    
+
+
+a = lambda b: b*100
+print(a(90))
+
+b = lambda c: c *500
+print(b(8))
+
+x = lambda a,b,c: (a * c) / b * a
+print(x(23,42,34))

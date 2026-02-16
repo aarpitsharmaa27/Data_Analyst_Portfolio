de7f96bdd99a81efc84bb109b1ab74ebd9fc9a66
aarpitsharmaa27 <aarpitsharmaa27@gmail.com>
2026-02-17 00:19:35 +0530

    Aggrigate function in array

---
+++ Aggregating_functions.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a = np.array([235,62,45,424,44,40])
+
+print(np.sum(a))
+print(np.min(a))
+print(np.max(a))
+print(np.size(a))
+print(np.mean(a))
+print(np.cumsum(a))  # adding values one by one
+print(np.cumprod(a))
+
